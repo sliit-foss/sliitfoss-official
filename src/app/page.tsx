5aff35cacf4a9e2ae751dbe2bcf6bf6428342616
Sadeesha Perera <121984253+SadeeshaPerera@users.noreply.github.com>
2025-10-19 21:37:23 +0000

    Feat: add gallery heading gradient and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import gallery from '@/data/gallery.json';
 export default function Home() {
   return (
     <main className="p-8">
-      <h1 className="mb-6 text-2xl font-bold">Gallery</h1>
+      <h1 className="mb-6 text-2xl font-bold font-bricolage heading-gradient">
+        Gallery
+      </h1>
       <div className="h-[80vh]">
         <Masonry items={gallery} />
       </div>
