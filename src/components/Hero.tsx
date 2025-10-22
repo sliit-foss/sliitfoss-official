eace09dc44927c304af9fafedcb99f0a858f8479
Seniru <senirupasan@gmail.com>
2025-10-22 19:18:52 +0530

    Feat: mobile friendly

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="h-screen relative">
       <div className="relative h-full grid content-center-safe z-40">
-        <h1 className="grid justify-center font-bold text-9xl font-sans mt-30 text-transparent bg-clip-text bg-gradient-to-r from-white to-[#0094BE]">
+        <h1 className="grid justify-center font-bold text-5xl md:text-7xl lg:text-9xl font-sans mt-30 text-transparent bg-clip-text bg-gradient-to-r from-white to-[#0094BE]">
           SLIIT FOSS
         </h1>
         <p className="text-center font-normal text-lg text-[#757575] m-6">
@@ -13,15 +13,15 @@ export default function Hero() {
           develop and diversify the usage of Free and Open Source Software at
           SLIIT.
         </p>
-        <div className="flex justify-center gap-[10px] mt-10">
-          <button className="cursor-pointer rounded-3xl py-[12px] px-[20px] shadow-[0_1px_4px_rgba(3,129,165,0.40)] text-[#30839B]">
+        <div className="grid gap-4 md:flex md:justify-center md:gap-[10px] mt-10 mb-10 mx-10 md:mb-0">
+          <button className="w-full md:w-auto cursor-pointer rounded-3xl py-[12px] px-[20px] shadow-[0_1px_4px_rgba(3,129,165,0.40)] text-[#30839B]">
             Become a member
           </button>
-          <button className="cursor-pointer rounded-3xl py-[12px] px-[20px] bg-gradient-to-r from-[#39ACCD] to-[#3FAECE] text-white">
+          <button className="w-full md:w-auto cursor-pointer rounded-3xl py-[12px] px-[20px] bg-gradient-to-r from-[#39ACCD] to-[#3FAECE] text-white">
             Become a member
           </button>
         </div>
-        <h1 className="grid justify-center font-bold text-9xl font-sans text-transparent bg-clip-text bg-gradient-to-r from-[#F9FDFF] to-[#0094BE] tracking-widest self-end-safe">
+        <h1 className="grid justify-center font-bold text-5xl md:text-7xl lg:text-9xl font-sans text-transparent bg-clip-text bg-gradient-to-r from-[#F9FDFF] to-[#0094BE] tracking-widest self-end-safe">
           community
         </h1>
       </div>
