d1ef9c599e8e1dfe7ad9fa0272eefd8c78976933
Seniru <senirupasan@gmail.com>
2025-10-22 19:07:37 +0530

    Feat: hero design

---
+++ src/components/Hero.tsx
@@ -0,0 +1,33 @@
+import Background from './Background';
+
+export default function Hero() {
+  return (
+    <section className="h-screen relative">
+      <div className="relative h-full grid content-center-safe z-40">
+        <h1 className="grid justify-center font-bold text-9xl font-sans mt-30 text-transparent bg-clip-text bg-gradient-to-r from-white to-[#0094BE]">
+          SLIIT FOSS
+        </h1>
+        <p className="text-center font-normal text-lg text-[#757575] m-6">
+          A group of volunteers who believe in the usage of Free & Open Source
+          Software (FOSS). The primary objective of the community is to promote,
+          develop and diversify the usage of Free and Open Source Software at
+          SLIIT.
+        </p>
+        <div className="flex justify-center gap-[10px] mt-10">
+          <button className="cursor-pointer rounded-3xl py-[12px] px-[20px] shadow-[0_1px_4px_rgba(3,129,165,0.40)] text-[#30839B]">
+            Become a member
+          </button>
+          <button className="cursor-pointer rounded-3xl py-[12px] px-[20px] bg-gradient-to-r from-[#39ACCD] to-[#3FAECE] text-white">
+            Become a member
+          </button>
+        </div>
+        <h1 className="grid justify-center font-bold text-9xl font-sans text-transparent bg-clip-text bg-gradient-to-r from-[#F9FDFF] to-[#0094BE] tracking-widest self-end-safe">
+          community
+        </h1>
+      </div>
+      <div className="absolute left-1/2 -translate-x-1/2 top-[-200px] w-1/3 h-[250px] blur-[20px] bg-[rgba(89,219,255,0.18)] rounded-2xl z-1" />
+      <Background />
+      <div className="absolute left-0 bottom-0 w-full h-[200px] blur-[10px] bg-[rgba(89,219,255,0.18)] rounded-full z-1" />
+    </section>
+  );
+}
